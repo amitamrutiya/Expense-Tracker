a6e1b771fc2ca805170c6a5ec76446c501941c1c
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-04-15 15:30:37 +0530

    create login page
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 import Header from "./components/ui/Header";
 
 function App() {
-  const authUser = true;
+  const authUser = false;
   return (
     <>
       {authUser && <Header />}
